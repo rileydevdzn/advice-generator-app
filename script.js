017a384726d51e648152c3d053ba48d8dcc1523f
rileydevdzn <111300135+rileydevdzn@users.noreply.github.com>
2023-03-23 17:48:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function updateCounter() {
 
 // Add event listeners
 
-newAdvice.addEventListener('click', () => {
+newAdvice.addEventListener("click", () => {
   getAdvice();
   updateCounter();
 });
